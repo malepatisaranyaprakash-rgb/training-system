eacdf211882e576ee51b54a6570c3795a1cac2c9
Malepati Saranya Praksh <malepatisaranyaprakash@gmail.com>
2026-05-11 22:44:23 +0530

    fixed app routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,35 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Trainings from './pages/Trainings';
 import TrainerDashboard from './pages/TrainerDashboard';
 import MyEnrollments from './pages/MyEnrollments';
 
+import './App.css';
+
 function App() {
+
   return (
+
     <BrowserRouter>
 
       <Routes>
 
-        <Route
-          path="/"
-          element={<Login />}
-        />
+        <Route path="/" element={<Login />} />
+
+        <Route path="/trainings" element={<Trainings />} />
+
+        <Route path="/trainer" element={<TrainerDashboard />} />
 
         <Route
-          path="/trainings"
-          element={<Trainings />}
-        />
-
-        <Route
-          path="/trainer"
-          element={<TrainerDashboard />}
+          path="/my-enrollments"
+          element={<MyEnrollments />}
         />
 
       </Routes>
 
     </BrowserRouter>
+
   );
 }
 
