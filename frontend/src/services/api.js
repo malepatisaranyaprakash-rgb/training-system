91b8b96ca76d79515f9b2d20a4edce6db0f1c293
Malepati Saranya Praksh <malepatisaranyaprakash@gmail.com>
2026-05-12 08:09:06 +0530

    fixed api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://training-system-1-lead.onrender.com/'
+  baseURL: 'https://training-system-1-lead.onrender.com/api'
 });
 
 API.interceptors.request.use((req) => {
